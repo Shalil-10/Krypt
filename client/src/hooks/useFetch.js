60806d6a2b95b97153353290b8303ea91d03e2d8
Shalil Dsilva <dsilvashalil@gmail.com>
2022-05-23 19:06:32 +0530

    commit 4

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
-
+const APIKEY = process.env.REACT_APP_GIPHY_API;
 const useFetch = ({ keyword }) => {
     const [gifUrl, setGifUrl] = useState("")
     const fetchGifs = async () => {
         try {
-            const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API}&q=${keyword.split(" ").join("")}&limit=1`)
+            const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${keyword.split(" ").join("")}&limit=1`)
             const { data } = await response.json()
             setGifUrl(data[0]?.images?.downsized_medium.url)
         } catch (error) {
